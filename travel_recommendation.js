6651326940cf8315523b935bdf49591f0e841cea
IlyaShelestov <shelestov_ilya2905@mail.ru>
2024-08-07 13:38:02 +0500

    Started JavaScript logic, added script to home page

---
+++ travel_recommendation.js
@@ -0,0 +1,20 @@
+const btn_clear = document.getElementById("btn-clear");
+const btn_search = document.getElementById("btn-search");
+
+async function fetchData() {
+    const response = await fetch("travel_recommendation_api.json");
+    const data = response.json();
+    return data;
+}
+
+async function search() {
+    const data = await fetchData();
+    console.log(data.countries[0].name);
+}
+
+function clear() {
+    document.getElementById("search-input").value = "";
+}
+
+btn_clear.addEventListener("click", clear);
+btn_search.addEventListener("click", search);
